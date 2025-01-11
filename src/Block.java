0afa5efcfaaf34290be106c1aae0ae5c9753ea1b
Marko <marko61680@gmail.com>
2025-01-11 14:10:38 -0500

    Start mining with mineBlock method

--- src/Block.java
+++ src/Block.java
@@ -7,6 +7,7 @@ public class Block {
 	public String previousHash; //hold the previous block’s hash
 	private String data; //our data will be a simple message & hold our block data.
 	private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
 
 	//Block Constructor.
 	public Block(String data,String previousHash ) {
@@ -16,7 +17,7 @@ public class Block {
         this.hash = calculateHash();
 	}
     public String calculateHash() {
-        //calculate the hash from all parts of the block we don’t want to be tampered with.
+        //calculate the new hash from all parts of the block we don’t want to be tampered with.
         String calculatedhash = StringUtil.applySha256( 
                 previousHash +
                 Long.toString(timeStamp) +
@@ -24,4 +25,19 @@ public class Block {
                 );
         return calculatedhash;
     }
+    public void mineBlock(int difficulty) {
+        //In practice each miner will start iterating from a random point. 
+        //Some miners may even try random numbers for nonce
+        //mineBlock() method takes in an int called difficulty, this is the number of 0’s they must solve for
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+    }
+
+
+
+
 }
